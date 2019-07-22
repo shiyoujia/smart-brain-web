37cb89d991f7a1774282d4d5199922bab5af9594
YouJia Shhi <shi.shiyoujia@gmail.com>
2019-07-22 00:44:59 -0700

    fix fetch

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
 
   onButtonSumbit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://murmuring-plateau-23478.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -91,7 +91,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://murmuring-plateau-23478.herokuapp.com:3000/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
         this.setState({ signInPassword: event.target.value })
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://murmuring-plateau-23478.herokuapp.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
